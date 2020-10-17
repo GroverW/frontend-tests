61fc45a0fcbc7e220d18b9ce74ff60c6db22fafc
GroverW <cheesemaker@gmail.com>
2020-10-16 18:53:26 -0700

    added getResults method and GifList component import for SearchGiphy

--- giphy-client/src/SearchGiphy/index.js
+++ giphy-client/src/SearchGiphy/index.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import SearchForm from '../SearchForm';
+import GifList from '../GifList';
+import { API_URL } from './constants';
 
 export default function Search() {
-  const [results, setResults] = useState(false);
+  const [results, setResults] = useState([]);
 
-  const getResults = (evt) => {
-
+  const getResults = (query) => {
+    fetch(`${API_URL}&q=${query}`)
+      .then((response) => response.json())
+      .then((data) => setResults(data.results))
+      .catch((error) => console.log(error));
   }
 
   return (
     <div className="Search">
-      <SearchForm getResults={getResults}
+      <SearchForm getResults={getResults} />
+      {Boolean(results.length) && <GifList gifs={results} />}
     </div>
   )
 }
