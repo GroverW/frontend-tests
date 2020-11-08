b99e4453b22cc076a5dd7805c2eabbf6c4acc15d
GroverW <cheesemaker@gmail.com>
2020-11-07 19:26:27 -0800

    moved mr-48 class from link to li in master layout

--- laravel-test/resources/views/layouts/master.blade.php
+++ laravel-test/resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <nav class="relative md:fixed flex justify-center items-center w-screen bg-gray-800  h-20 md:h-32">
       <ul class="hidden md:flex flex-row">
         <li><a class="block {{ Request::path() == '/' ? 'text-red-600' : 'text-gray-500 hover:text-gray-300 hover:bg-gray-900' }} uppercase w-24 text-center leading-4 p-2 rounded" href="">Home</a></li>
-        <li><a class="block {{ Request::path() == 'menu' ? 'text-red-600' : 'text-gray-500 hover:text-gray-300 hover:bg-gray-900' }} uppercase w-24 text-center leading-4 p-2 rounded mr-48" href="">Menu</a></li>
+        <li class="mr-48"><a class="block {{ Request::path() == 'menu' ? 'text-red-600' : 'text-gray-500 hover:text-gray-300 hover:bg-gray-900' }} uppercase w-24 text-center leading-4 p-2 rounded" href="">Menu</a></li>
         <li><a class="block {{ Request::path() == 'about' ? 'text-red-600' : 'text-gray-500 hover:text-gray-300 hover:bg-gray-900' }} uppercase w-24 text-center leading-4 p-2 rounded" href="">About</a></li>
         <li><a class="block {{ Request::path() == 'contact' ? 'text-red-600' : 'text-gray-500 hover:text-gray-300 hover:bg-gray-900' }} uppercase w-24 text-center leading-4 p-2 rounded" href="">Contact</a></li>
       </ul>
